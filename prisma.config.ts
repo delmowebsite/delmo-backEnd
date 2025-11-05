057f5573cc7aa2b923a6bcf1ab1a4c07ca8fe72d
javad-reazaie <111302208+JAVADking4259@users.noreply.github.com>
2025-11-05 15:06:55 +0330

    ref: refactor prisma and change database to mongodb and initilize prisma service

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,12 +1,12 @@
-import { defineConfig, env } from "prisma/config";
-
-export default defineConfig({
-  schema: "prisma/schema.prisma",
-  migrations: {
-    path: "prisma/migrations",
-  },
-  engine: "classic",
-  datasource: {
-    url: env("DATABASE_URL"),
-  },
-});
+// import { defineConfig, env } from 'prisma/config';
+//
+// export default defineConfig({
+//   schema: 'prisma/schema/schema.prisma',
+//   migrations: {
+//     path: 'prisma/migrations',
+//   },
+//   engine: 'classic',
+//   datasource: {
+//     url: env('DATABASE_URL'),
+//   },
+// });
---
+++ src/prisma/prisma.service.ts
@@ -0,0 +1,11 @@
+import { Injectable, OnModuleInit, Logger } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class PrismaService extends PrismaClient implements OnModuleInit {
+  async onModuleInit() {
+    await this.$connect().then(() => {
+      new Logger().log(`Database connected to Prisma Service`);
+    });
+  }
+}
