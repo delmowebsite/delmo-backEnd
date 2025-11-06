760817c6008f776a74f2121ddb1ab83310781b39
javad-reazaie <111302208+JAVADking4259@users.noreply.github.com>
2025-11-06 10:24:55 +0330

    feat: add user and role models

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from '@prisma/config';
+import { defineConfig, env } from '@prisma/config';
 
 export default defineConfig({
-  schema: './prisma/schema/schema.prisma',
+  schema: './prisma/schema.prisma',
 });
 
 // import { defineConfig, env } from 'prisma/config';
